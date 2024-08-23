aef0856279e16af59804bba59077f63f268ab79a
Ng989 <00001085202513sp@al.educacao.sp.gov.br>
2024-08-23 20:11:38 -0300

    Adicionando arquivo

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(600, 600);
+    brackground("white")
+  {
+    
+    function draw(){
+  
+      stroke("blue")
+      fill("red")
+      
+      // console.log(mouselsPressed)
+      
+    if (mouselsPressed){
+      rect(mouseX,mouseY)
+     }
+      
+   }
+    
